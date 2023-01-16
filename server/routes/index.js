695e44d1fdbe11461a69602ae226929a9a55b64d
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-01-16 23:56:55 +0300

    d

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,14 @@ const deviceRouter = require('./deviceRouter')
 const userRouter = require('./userRouter')
 const brandRouter = require('./brandRouter')
 const typeRouter = require('./typeRouter')
+const errorHandler = require('../middleware/ErrorHandlingMiddleware')
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
 router.use('/device', deviceRouter)
+
+//Обработка ошибкок, последнйи Middleware
+router.use(errorHandler)
 
 module.exports = router
