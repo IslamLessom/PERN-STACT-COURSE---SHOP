08920f7bef13fc21d3fa31c7ad4fe653b57662cf
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-01-17 23:49:28 +0300

    s

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
+const path = require('path')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
